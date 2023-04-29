eed146ac35b3afca9b9166d1b8e40add27f651b8
Luiz Filipe <luiz.lopes@get.inatel.br>
2023-04-29 00:47:37 -0300

    Removendo redundancia

--- src/br/luizfilipels/calcipv4/Main.java
+++ src/br/luizfilipels/calcipv4/Main.java
@@ -1,2 +1,14 @@
-package br.luizfilipels.calcipv4;public class Main {
+package br.luizfilipels.calcipv4;
+
+import br.luizfilipels.calcipv4.conversor.Conversor_Ipv4;
+public class Main {
+    public static void main(String[] args) {
+        String ip = "192.168.0.1";
+
+        System.out.println("Calculadora IPv4");
+        System.out.println("Enviando o ip: 192.168.0.1");
+
+        System.out.println(Conversor_Ipv4.converter_ip(ip));
+    }
 }
+
--- src/br/luizfilipels/calcipv4/conversor/Conversor_Ipv4.java
+++ src/br/luizfilipels/calcipv4/conversor/Conversor_Ipv4.java
@@ -1,2 +1,19 @@
-package br.luizfilipels.calcipv4.conversor;public class Conversor_Ipv4 {
+package br.luizfilipels.calcipv4.conversor;
+
+public class Conversor_Ipv4 {
+
+    public static String converter_ip(String ipv4) {
+        String[] octetos = ipv4.split("\\.");
+        StringBuilder OctetosBinarios = new StringBuilder();
+        for (String octeto : octetos) {
+            int decimal = Integer.parseInt(octeto);
+            String binary = Integer.toBinaryString(decimal);
+            OctetosBinarios.append(String.format("%8s", binary).replaceAll(" ", "0")).append(".");
+        }
+        // Remove the trailing dot
+        OctetosBinarios.deleteCharAt(OctetosBinarios.length() - 1);
+
+        return OctetosBinarios.toString();
+    }
+
 }
